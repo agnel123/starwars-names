a01a87772fca3a69cb8848a6177afdb7f16e2688
PjCox <philipcox83@gmail.com>
2015-10-18 15:02:07 +0100

    updates to names.

--- src/index.js
+++ src/index.js
@@ -14,4 +14,11 @@ module.exports = {
   Keep your npm version and Githib versions up-to-date
   by adding a git-tag to your repo.
   A tag points to a particular commit.
+
+  Version conventions
+
+  1.0.0
+  1, would be changed to two if we make breaking changes like method name changes.
+  first 0, is a minor release, new features, but no breaking changes.
+  second 0, is for patch releases, bug fixes etc.
 */
